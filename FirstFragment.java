f0d8c12e710a59013c9c6c1fe023e008e39a0399
tateiwagit <tatitatagoo@gmail.com>
2025-04-02 22:58:00 +0900

    add:カウンター機能の追加・音声ボタン押下検出
    画面上でカウンター表示を操作するボタンを追加
    音声ボタン操作を検出　ただし，音量が変更したことに反応している

--- FirstFragment.java
+++ FirstFragment.java
@@ -1,19 +1,32 @@
 package com.example.mycounter;
 
+import android.content.Context;
+import android.database.ContentObserver;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.mycounter.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private TextView counterText;
+    private int counter = 0;
+    private VolumeContentObserver volumeObserver;
+    private Handler androidHandler = new Handler();
+
 
     @Override
     public View onCreateView(
@@ -29,9 +42,34 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(v ->
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        requireActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        counterText = view.findViewById(R.id.counter_text);
+
+        Button plusButton = view.findViewById(R.id.plus_button);
+        plusButton.setOnClickListener(v -> {
+            counter++;
+            counterText.setText(String.valueOf(counter));
+        });
+
+        Button minusButton = view.findViewById(R.id.minus_button);
+        minusButton.setOnClickListener(v -> {
+            counter--;
+            counterText.setText(String.valueOf(counter));
+        });
+
+        Button resetButton = view.findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(v -> {
+            counter = 0;
+            counterText.setText(String.valueOf(counter));
+        });
+
+        // Initialize and register VolumeContentObserver
+        volumeObserver = new VolumeContentObserver(requireContext(), androidHandler);
+        requireActivity().getContentResolver().registerContentObserver(
+                android.provider.Settings.System.CONTENT_URI,
+                true,
+                volumeObserver
         );
     }
 
@@ -39,6 +77,8 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        // Unregister VolumeContentObserver
+        requireActivity().getContentResolver().unregisterContentObserver(volumeObserver);
     }
 
 }
---
+++ VolumeContentObserver.java
@@ -0,0 +1,42 @@
+package com.example.mycounter;
+
+import android.content.Context;
+import android.database.ContentObserver;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.util.Log;
+
+public class VolumeContentObserver extends ContentObserver {
+
+    private static final String TAG = "VolumeObserver";
+    private Context context;
+    private AudioManager audioManager;
+    private int previousVolume;
+
+    public VolumeContentObserver(Context context, Handler handler) {
+        super(handler);
+        this.context = context;
+        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        previousVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+    }
+
+    @Override
+    public void onChange(boolean selfChange) {
+        super.onChange(selfChange);
+        onChange(selfChange, null);
+    }
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        System.out.print("onChange");
+        if (currentVolume != previousVolume) {
+            Log.d(TAG, "Volume changed from " + previousVolume + " to " + currentVolume);
+            // Implement your counter logic here
+            // For example:
+            // ((MainActivity) context).updateCounterBasedOnVolumeChange(currentVolume - previousVolume);
+            previousVolume = currentVolume;
+        }
+    }
+}
